bb1234227eb549ba1e34a76dcffc709aaa5f46b1
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2023-05-08 21:10:49 -0300

    Payment, one-to-one association

--- src/main/java/com/nandoamori/course/entities/Order.java
+++ src/main/java/com/nandoamori/course/entities/Order.java
@@ -26,6 +26,9 @@ public class Order implements Serializable {
     private User client;
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -73,6 +76,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
