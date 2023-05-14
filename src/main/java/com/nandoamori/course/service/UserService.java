5039f7648d193d5b6817e8a2a6e53f5359e704a2
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2023-05-14 13:34:29 -0300

    Exception handling - update

--- src/main/java/com/nandoamori/course/service/UserService.java
+++ src/main/java/com/nandoamori/course/service/UserService.java
@@ -4,6 +4,7 @@ import com.nandoamori.course.entities.User;
 import com.nandoamori.course.repository.UserRepository;
 import com.nandoamori.course.service.exceptions.DatabaseException;
 import com.nandoamori.course.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -40,9 +41,13 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
     private void updateData(User entity, User user){
         entity.setName(user.getName());
